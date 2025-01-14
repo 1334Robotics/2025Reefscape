48654a1f6583fee4c7245f1c2fbaab45bda925c3
ArjunSaini307 <122317857+ArjunSaini307@users.noreply.github.com>
2025-01-14 15:27:04 -0500

    Create PIDSubsystem.java

---
+++ src/main/java/frc/robot/subsystems/drive/PIDSubsystem.java
@@ -0,0 +1,40 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+// Need photon vision data
+
+public class PIDSubsystem extends SubsystemBase {
+  
+  private PID pidTheta,pidY;  // PID controller for theta and Y, could add X later
+  
+  private double setPointTheta = 0.0;
+  private double setPointY = 0.0;
+  
+  
+  public AprilTagTrackingSubsystem() {
+    pidY = new PID(0.1, 0.01, 0.0, 0.0, -1.0, 1.0, -0.5, 0.5, 0.02);  // Example PID parameters
+    pidTheta = new PID(0.1, 0.01, 0.0, 0.0, -1.0, 1.0, -0.5, 0.5, 0.02);
+  }
+  
+  @Override
+  public void periodic() {
+    // Get data from PhotonVision
+    int y = 0;
+    int theta = 0;
+    pidY.update(setPointY, y);
+    pidTheta.update(setPointTheta, theta);
+
+    double outputY = pidY.getSteer();
+    double outputTheta = pidTheta.getSteer();
+    
+      // Use the outputs to control the robot's movement with driveSubsystem
+    }
+  }
+
+
+  @Override
+  public void simulationPeriodic() {
+    // Called during simulation
+  }
+}
