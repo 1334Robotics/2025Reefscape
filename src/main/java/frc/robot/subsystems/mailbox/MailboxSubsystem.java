a10e62ec41c41b85e1394d4505c1d07d9eb7ce70
Evan <114953191+aesom-e@users.noreply.github.com>
2025-01-30 11:30:21 -0500

    Fixed MailboxSubsystem to work on actual hardware

--- src/main/java/frc/robot/subsystems/mailbox/MailboxSubsystem.java
+++ src/main/java/frc/robot/subsystems/mailbox/MailboxSubsystem.java
@@ -1,55 +1,54 @@
 package frc.robot.subsystems.mailbox;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix6.hardware.TalonFXS;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.MailboxConstants;
 
 public class MailboxSubsystem extends SubsystemBase {
-    private final TalonSRX[] motors;
+    private final TalonFXS[] motors;
 
     public MailboxSubsystem() {
-        this.motors = new TalonSRX[2];
-        this.motors[0] = new TalonSRX(MailboxConstants.MOTOR_ONE_ID);
-        this.motors[1] = new TalonSRX(MailboxConstants.MOTOR_TWO_ID);
+        this.motors = new TalonFXS[2];
+        this.motors[0] = new TalonFXS(MailboxConstants.MOTOR_ONE_ID);
+        this.motors[1] = new TalonFXS(MailboxConstants.MOTOR_TWO_ID);
         SmartDashboard.putString("[MAILBOX] State", "Unknown");
         SmartDashboard.putNumber("[MAILBOX] Motor One Speed", 0);
         SmartDashboard.putNumber("[MAILBOX] Motor Two Speed", 0);
     }
 
     public MailboxSubsystem(int motorOneId, int motorTwoId) {
-        this.motors = new TalonSRX[2];
-        this.motors[0] = new TalonSRX(motorOneId);
-        this.motors[1] = new TalonSRX(motorTwoId);
+        this.motors = new TalonFXS[2];
+        this.motors[0] = new TalonFXS(motorOneId);
+        this.motors[1] = new TalonFXS(motorTwoId);
         SmartDashboard.putString("[MAILBOX] State", "Unknown");
         SmartDashboard.putNumber("[MAILBOX] Motor One Speed", 0);
         SmartDashboard.putNumber("[MAILBOX] Motor Two Speed", 0);
     }
 
     public void input() {
-        this.motors[0].set(ControlMode.PercentOutput, MailboxConstants.INPUT_MOTOR_ONE_SPEED);
-        this.motors[1].set(ControlMode.PercentOutput, MailboxConstants.INPUT_MOTOR_TWO_SPEED);
+        this.motors[0].set(MailboxConstants.INPUT_MOTOR_ONE_SPEED);
+        this.motors[1].set(MailboxConstants.INPUT_MOTOR_TWO_SPEED);
         SmartDashboard.putString("[MAILBOX] State", "Input");
     }
 
     public void output() {
         // This will need limit switches or other timings to tell when output is complete
-        this.motors[0].set(ControlMode.PercentOutput, MailboxConstants.OUTPUT_MOTOR_SPEED);
-        this.motors[1].set(ControlMode.PercentOutput, MailboxConstants.OUTPUT_MOTOR_SPEED);
+        this.motors[0].set(MailboxConstants.OUTPUT_MOTOR_SPEED);
+        this.motors[1].set(MailboxConstants.OUTPUT_MOTOR_SPEED);
         SmartDashboard.putString("[MAILBOX] State", "Output");
     }
 
     public void stop() {
-        this.motors[0].set(ControlMode.PercentOutput, 0);
-        this.motors[1].set(ControlMode.PercentOutput, 0);
+        this.motors[0].set(0);
+        this.motors[1].set(0);
         SmartDashboard.putString("[MAILBOX] State", "Stopped");
     }
 
     @Override
     public void periodic() {
-        SmartDashboard.putNumber("[MAILBOX] Motor One Speed", this.motors[0].getMotorOutputPercent());
-        SmartDashboard.putNumber("[MAILBOX] Motor Two Speed", this.motors[1].getMotorOutputPercent());
+        SmartDashboard.putNumber("[MAILBOX] Motor One Speed", this.motors[0].get());
+        SmartDashboard.putNumber("[MAILBOX] Motor Two Speed", this.motors[1].get());
     }
 }
