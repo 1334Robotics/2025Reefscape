29b5be947b2eb356fd26d6fe55f40371a811c7aa
LunarcatOwO <168307748+MelodyDevelopment@users.noreply.github.com>
2025-03-18 17:13:28 -0400

    it builds, just not sure if it works or not

--- src/main/java/frc/robot/subsystems/climb/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/climb/ClimbSubsystem.java
@@ -5,6 +5,7 @@ import frc.robot.constants.ClimbConstants;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Encoder;
 
 public class ClimbSubsystem extends SubsystemBase {
     // Two lower Neo motors for forcing the pins down
@@ -13,20 +14,23 @@ public class ClimbSubsystem extends SubsystemBase {
     // One Neo 550 motor for locking the climb mechanism
     private final SparkMax lockMotor;
 
+    private final Encoder lowerMotor1Encoder = new Encoder(0, 1);
+    private final Encoder lowerMotor2Encoder = new Encoder(2, 3);
+    private final Encoder lockMotorEncoder = new Encoder(4, 5);
+
+    // Conversion factor from encoder ticks to inches
+    private static final double TICKS_TO_INCHES = 0.01; // Adjust this value based on your specific encoder and mechanism
+
     public ClimbSubsystem() {
         // Adjust CAN IDs as needed based on your wiring
         lowerMotor1 = new SparkMax(ClimbConstants.LOWER_MOTOR1_CAN_ID, MotorType.kBrushless);
         lowerMotor2 = new SparkMax(ClimbConstants.LOWER_MOTOR2_CAN_ID, MotorType.kBrushless);
         lockMotor   = new SparkMax(ClimbConstants.LOCK_MOTOR_CAN_ID, MotorType.kBrushless);
 
-        // // Restore defaults and set idle mode to brake for better hold
-        // lowerMotor1.restoreFactoryDefaults();
-        // lowerMotor2.restoreFactoryDefaults();
-        // lockMotor.restoreFactoryDefaults();
-        
-        // lowerMotor1.setIdleMode(SparkMax.IdleMode.kBrake);
-        // lowerMotor2.setIdleMode(SparkMax.IdleMode.kBrake);
-        // lockMotor.setIdleMode(SparkMax.IdleMode.kBrake);
+        // Initialize encoders if needed
+        lowerMotor1Encoder.setDistancePerPulse(TICKS_TO_INCHES);
+        lowerMotor2Encoder.setDistancePerPulse(TICKS_TO_INCHES);
+        lockMotorEncoder.setDistancePerPulse(TICKS_TO_INCHES);
     }
 
     /**
@@ -67,6 +71,18 @@ public class ClimbSubsystem extends SubsystemBase {
         lowerMotor2.set(0);
         lockMotor.set(0);
         SmartDashboard.putString("[Climb] State", "Stopped");
+    }
+
+    public double getLowerMotor1PositionInches() {
+        return lowerMotor1Encoder.getDistance();
+    }
+
+    public double getLowerMotor2PositionInches() {
+        return lowerMotor2Encoder.getDistance();
+    }
+
+    public double getLockMotorPositionInches() {
+        return lockMotorEncoder.getDistance();
     }
 
     @Override
