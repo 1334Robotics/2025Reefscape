e04b43b20179df26cee85b7066e683b4836991d7
LunarcatOwO <168307748+MelodyDevelopment@users.noreply.github.com>
2025-03-24 18:58:27 -0400

    Update photonlib version to v2025.2.1 and add VisionPoseEstimator subsystem for improved pose estimation

---
+++ src/main/java/frc/robot/subsystems/vision/VisionPoseEstimator.java
@@ -0,0 +1,45 @@
+package frc.robot.subsystems.vision;
+
+import java.util.Optional;
+import java.util.List;
+
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+import org.photonvision.targeting.PhotonPipelineResult;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class VisionPoseEstimator extends SubsystemBase {
+    private final PhotonCamera cam;
+    private final PhotonPoseEstimator photonPoseEstimator;
+    public VisionPoseEstimator() {
+        cam = new PhotonCamera("Arducam_OV9782_USB_Camera");
+        Transform3d robotToCam = new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0,0,0)); //Cam mounted facing forward, half a meter forward of center, half a meter up from center.
+            //CHANGE TO ACTURAL VALUES VERY IMPORTANT, We also seemed to missed this when we made the first one
+        AprilTagFieldLayout aprilTagFieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2025ReefscapeWelded);
+        photonPoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, robotToCam);
+    }
+    public Optional<EstimatedRobotPose> getEstimatedGlobalPose(Pose2d prevEstimatedRobotPose) {
+        photonPoseEstimator.setReferencePose(prevEstimatedRobotPose);
+        
+        // Get all unread results
+        List<PhotonPipelineResult> results = cam.getAllUnreadResults();
+        
+        // Use the latest result if available
+        if (!results.isEmpty()) {
+            return photonPoseEstimator.update(results.get(results.size() - 1));
+        }
+        
+        // Return an empty Optional if no results are available
+        return Optional.empty();
+    }    
+}
